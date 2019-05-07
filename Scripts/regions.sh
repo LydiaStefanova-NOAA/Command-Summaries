09c51086fa5dd1b0ff185aceb32411860c1dde4b
LydiaStefanova-NOAA <48454097+LydiaStefanova-NOAA@users.noreply.github.com>
2019-05-07 14:58:25 -0400

    Create regions.sh

---
+++ Scripts/regions.sh
@@ -0,0 +1,29 @@
+# Global
+  region="GLO"; LonE=0. ; LonW=360. ; LatS=-90. ; LatN=90.
+
+# Northern Hemisphere Extratropics
+  region="NHX"; LonE=0. ; LonW=360. ; LatS=20. ; LatN=80.
+
+# Souther Hemisphere Extratropics
+  region="SHX"; LonE=0. ; LonW=360. ; LatS=-80. ; LatN=20.
+
+# Tropics
+  region="TRO"; LonE=0. ; LonW=360. ; LatS=-20. ; LatN=20.
+
+# Nino 3.4
+  region="Nino34"; LonE=170. ; LonW=240. ; LatS=-5. ; LatN=5.
+
+# PNA
+  region="PNA"; LonE=180. ; LonW=320. ; LatS=20. ; LatN=75.
+
+# CONUS
+  region="CONUS"; LonE=235. ; LonW=294. ; latS=24 ; LatN=50.
+
+# Europe
+  region="EUR"; LonE=329. ; LonW=40. ; latS=27 ; LatN=81.
+
+# South America
+  region="SAM"; LonE=250. ; LonW=334. ; LatS=-60.; LatN=14.
+
+# Amazon
+  region="AMZ"; LonE=280. ; lonW=316. ; latS=-18. ; LatN=9.
