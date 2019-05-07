5ab08be8ca96be1a23ca91a775abe09e5497cc48
LydiaStefanova-NOAA <48454097+LydiaStefanova-NOAA@users.noreply.github.com>
2019-05-07 14:59:46 -0400

    Update regions.sh

--- Scripts/regions.sh
+++ Scripts/regions.sh
@@ -1,29 +1,29 @@
 # Global
-  region="GLO"; LonE=0. ; LonW=360. ; LatS=-90. ; LatN=90.
+  region="GLO"; LonE=000. ; LonW=360. ; LatS=-90. ; LatN=90.
 
 # Northern Hemisphere Extratropics
-  region="NHX"; LonE=0. ; LonW=360. ; LatS=20. ; LatN=80.
+  region="NHX"; LonE=000. ; LonW=360. ; LatS=20. ; LatN=80.
 
 # Souther Hemisphere Extratropics
-  region="SHX"; LonE=0. ; LonW=360. ; LatS=-80. ; LatN=20.
+  region="SHX"; LonE=000. ; LonW=360. ; LatS=-80. ; LatN=20.
 
 # Tropics
-  region="TRO"; LonE=0. ; LonW=360. ; LatS=-20. ; LatN=20.
+  region="TRO"; LonE=000. ; LonW=360. ; LatS=-20. ; LatN=20.
 
 # Nino 3.4
-  region="Nino34"; LonE=170. ; LonW=240. ; LatS=-5. ; LatN=5.
+  region="N34"; LonE=170. ; LonW=240. ; LatS=-05. ; LatN=05.
 
 # PNA
   region="PNA"; LonE=180. ; LonW=320. ; LatS=20. ; LatN=75.
 
 # CONUS
-  region="CONUS"; LonE=235. ; LonW=294. ; latS=24 ; LatN=50.
+  region="USA"; LonE=235. ; LonW=294. ; latS=24 ; LatN=50.
 
 # Europe
-  region="EUR"; LonE=329. ; LonW=40. ; latS=27 ; LatN=81.
+  region="EUR"; LonE=329. ; LonW=040. ; latS=27 ; LatN=81.
 
 # South America
   region="SAM"; LonE=250. ; LonW=334. ; LatS=-60.; LatN=14.
 
 # Amazon
-  region="AMZ"; LonE=280. ; lonW=316. ; latS=-18. ; LatN=9.
+  region="AMZ"; LonE=280. ; LonW=316. ; LatS=-18. ; LatN=09.
